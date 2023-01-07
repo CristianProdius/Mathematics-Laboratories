f185da461f1675b0f0ddc5065ad189fa00c19c99
CristianProdius <114479945+CristianProdius@users.noreply.github.com>
2023-01-07 05:10:58 +0200

    ad 10 modifyt 8

---
+++ lab2/PSA/10ImportanDistribution.py
@@ -0,0 +1,30 @@
+import random
+
+couth_controlor_probability = 0.05
+must_pay_probability = 0.02
+
+the_rebel_one_for_1_year = 0
+count_cought_by_contolor = 0
+encounter_hairy_taxator = 0
+
+the_normal_ones_pay_for_1_year = 2*972
+for _ in range(700):
+    if random.choices(["good lady", "hary muscular ciuvac"], [1-must_pay_probability, must_pay_probability]) == ["hary muscular ciuvac"]:
+        the_rebel_one_for_1_year += 6
+        encounter_hairy_taxator += 1
+    else:
+        if random.choices(["free to ride", "cought"], [1-couth_controlor_probability, couth_controlor_probability]) == ["cought"]:
+            count_cought_by_contolor += 1
+            if count_cought_by_contolor == 1:
+                the_rebel_one_for_1_year += 50
+            elif count_cought_by_contolor == 2:
+                the_rebel_one_for_1_year += 150
+            else:
+                the_rebel_one_for_1_year += 300
+
+print("The rebel encounter a hairy taxator: ", encounter_hairy_taxator)
+print("The rebel student was cought by the contolor:", count_cought_by_contolor)
+print("Total sub that the rebel spended was: ", the_rebel_one_for_1_year)
+print("*******************************")
+print("Conclusion: ")
+print("the normal ones pay: ", the_normal_ones_pay_for_1_year, "which is with: ", the_rebel_one_for_1_year-the_normal_ones_pay_for_1_year, "less the the rebel one")
--- lab2/PSA/8Densities.py
+++ lab2/PSA/8Densities.py
@@ -4,14 +4,23 @@ def the_alone_one():
     lunch = random.sample(range(10), 10)
     dinner = random.sample(range(10), 10)
 
+    neighbors_lunch = []
+    neighbors_dinner = []
     for i in range(10):
-        if dinner[(i + 1) % 10] == lunch[(i + 1) % 10] or dinner[(i - 1) % 10] == lunch[i]:
+        neighbors_lunch.append([lunch[(i-1)%10], lunch[i], lunch[(i+1)%10]])
+    for i in range(10):
+        neighbors_dinner.append([dinner[(i-1)%10], dinner[i], dinner[(i+1)%10]])
+
+    for i in range(10):
+        if neighbors_lunch[i] in neighbors_dinner or neighbors_dinner[i] in neighbors_lunch:
             return False
     return True
 
-the_loners = 0
+
+
+arent_seting_at_both_lunch_and_dinner = 0
 for _ in range(100000):
     if the_alone_one():
-        the_loners += 1
+        arent_seting_at_both_lunch_and_dinner  += 1
 
-print(the_loners / 100000)
+print(arent_seting_at_both_lunch_and_dinner  / 100000)
